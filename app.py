a83f03528d1910dd924cc6a32c7598b65fe46285
LF <lf.04@hotmail.com>
2024-09-10 23:06:47 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from datetime import datetime, time
+import pandas as pd
+import numpy as np
+
+def main():
+    st.title("Sistema de CRM e Vendas")
+    email = st.text_input("Email", value="contato@leonardofernandes.com.br")
+    data = st.date_input("Data da venda", datetime.now())
+    hora = st.time_input("Hora da venda", value=time(9, 0)) #valor padrão 09:00
+    valor = st.number_input("Valor da venda", min_value=0.0, format="%.2f")
+    qtd = st.number_input("Quantidade de produtos", min_value=1, step=1, format="%d")
+    produto = st.selectbox("Produto", ["ZapFlow com Gemini", "ZapFlow com ChatGPT", "ZapFlow com LIamma3.0"])
+    
+    if st.button("Salvar"):
+        
+        data_hora = datetime.combine(data, hora)
+        st.write("**Dados da Venda:**")
+        st.write(f"Email: {email}")
+        st.write(f"Data e hora da venda: {data_hora}")
+        st.write(f"Valor: {valor}")
+        st.write(f"Quantidade: {qtd}")
+        st.write(f"Produto: {produto}")
+        
+
+    
+
+if __name__ == "__main__" :
+    main()
